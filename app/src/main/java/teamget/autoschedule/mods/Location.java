33a3eddb955d3cfafa17ad603135d7e67b2acca7
etlow <42794246+etlow@users.noreply.github.com>
2019-06-21 15:16:03 +0800

    Add locations and distances

--- app/src/main/java/teamget/autoschedule/mods/Location.java
+++ app/src/main/java/teamget/autoschedule/mods/Location.java
@@ -1,4 +1,22 @@
 package teamget.autoschedule.mods;
 
 public class Location {
+    private static double[][] distArr = {{0,457,520.9},{390.9,0,404.7},{503.2,471.5,0}};
+    private int ref;
+
+    public Location(String r) {
+        switch(r) {
+            case "UTown": ref = 0; break;
+            case "FoS": ref = 1; break;
+            case "SoC": ref = 2; break;
+        }
+    }
+
+    public double distanceTo(Location other) {
+        return distArr[ref][other.ref];
+    }
+
+    public String toString() {
+        return Integer.toString(ref);
+    }
 }
--- app/src/main/java/teamget/autoschedule/mods/SampleModules.java
+++ app/src/main/java/teamget/autoschedule/mods/SampleModules.java
@@ -7,20 +7,22 @@ import java.util.List;
 public class SampleModules {
     public static List<Module> getModules() {
         List<Module> list = new ArrayList<>();
-        Location loc = new Location();
+        Location loc0 = new Location("UTown");
+        Location loc1 = new Location("FoS");
+        Location loc2 = new Location("SoC");
 
-        Lesson a = new Lesson(1, 2, "Lecture", loc);
-        Lesson b = new Lesson(11, 12, "Lecture", loc);
+        Lesson a = new Lesson(1, 2, "Lecture", loc0);
+        Lesson b = new Lesson(11, 12, "Lecture", loc0);
         Option o1 = new Option(Arrays.asList(a, b));
 
-        Lesson c = new Lesson(2, 3, "Lecture", loc);
-        Lesson d = new Lesson(12, 13, "Lecture", loc);
+        Lesson c = new Lesson(2, 3, "Lecture", loc1);
+        Lesson d = new Lesson(12, 13, "Lecture", loc1);
         Option o2 = new Option(Arrays.asList(c, d));
 
         List<Option> lec = Arrays.asList(o1, o2);
 
-        Option ot1 = new Option(Arrays.asList(new Lesson(4, 5, "Tutorial", loc)));
-        Option ot2 = new Option(Arrays.asList(new Lesson(6, 7, "Tutorial", loc)));
+        Option ot1 = new Option(Arrays.asList(new Lesson(4, 5, "Tutorial", loc2)));
+        Option ot2 = new Option(Arrays.asList(new Lesson(6, 7, "Tutorial", loc2)));
 
         List<Option> tut = Arrays.asList(ot1, ot2);
 
