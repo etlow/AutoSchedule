43ba79d806ad81602f579dee51855b211e2d3119
etlow <42794246+etlow@users.noreply.github.com>
2019-06-23 18:57:44 +0800

    Check for clashes in timetable

--- app/src/main/java/teamget/autoschedule/schedule/Timetable.java
+++ app/src/main/java/teamget/autoschedule/schedule/Timetable.java
@@ -2,6 +2,7 @@ package teamget.autoschedule.schedule;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -20,40 +21,67 @@ public class Timetable {
     private List<Integer> pos = new ArrayList<>();
     private List<Lesson> table = new ArrayList<>();
     public boolean next(List<Module> modules) {
-        if (currList != -1) {
-            while (pos.get(pos.size() - 1) == modules.get(currModule).list.get(currList).size() - 1) {
-                int optNum = pos.remove(pos.size() - 1);
-                table.removeAll(modules.get(currModule).list.get(currList).get(optNum).list);
-                if (currList == 0) {
-                    if (currModule == 0) {
-                        return false;
+        boolean clash = true;
+        while (clash) {
+            clash = false;
+            if (currList != -1) { // Not the first call
+                // Keep removing elements from pos if it's already the last option
+                while (pos.get(pos.size() - 1) == modules.get(currModule).list.get(currList).size() - 1) {
+                    int optNum = pos.remove(pos.size() - 1);
+                    table.removeAll(modules.get(currModule).list.get(currList).get(optNum).list);
+                    if (currList == 0) {
+                        if (currModule == 0) {
+                            // No more possibilities
+                            return false;
+                        }
+                        currModule--;
+                        currList = modules.get(currModule).list.size();
                     }
-                    currModule--;
-                    currList = modules.get(currModule).list.size();
+                    currList--;
                 }
-                currList--;
+                // Update table to next possibility
+                int optNum = pos.get(pos.size() - 1);
+                pos.set(pos.size() - 1, optNum + 1);
+                table.removeAll(modules.get(currModule).list.get(currList).get(optNum).list);
+                Option newOption = modules.get(currModule).list.get(currList).get(optNum + 1);
+                // Check for clashes if newOption is added
+                clash = clashes(newOption);
+                table.addAll(newOption.list);
             }
-            int optNum = pos.get(pos.size() - 1);
-            pos.set(pos.size() - 1, optNum + 1);
-            table.removeAll(modules.get(currModule).list.get(currList).get(optNum).list);
-            table.addAll(modules.get(currModule).list.get(currList).get(optNum + 1).list);
-        }
-        boolean last = false;
-        while (!last) {
-            List<List<Option>> typeList = modules.get(currModule).list;
-            while (currList < typeList.size() - 1) {
-                pos.add(0);
-                currList++;
-                table.addAll(typeList.get(currList).get(0).list);
+            boolean last = false;
+            // Keep adding 0 to pos until last list of last module is reached
+            while (!clash && !last) {
+                List<List<Option>> typeList = modules.get(currModule).list;
+                while (!clash && currList < typeList.size() - 1) {
+                    pos.add(0);
+                    currList++;
+                    Option newOption = typeList.get(currList).get(0);
+                    clash = clashes(newOption);
+                    table.addAll(newOption.list);
+                }
+                if (!clash) {
+                    if (currModule == modules.size() - 1) {
+                        // Last list of last module
+                        last = true;
+                    } else {
+                        currModule++;
+                        currList = -1;
+                    }
+                }
             }
-            if (currModule == modules.size() - 1) {
-                last = true;
-            } else {
-                currModule++;
-                currList = -1;
-            }
+            Log.v("Timetable", pos.toString());
+            if (clash) Log.v("Timetable", "(Clash)");
         }
         return true;
+    }
+
+    private boolean clashes(Option option) {
+        for (Lesson lesson : option.list) {
+            for (Lesson tableLesson : table) {
+                if (lesson.overlaps(tableLesson)) return true;
+            }
+        }
+        return false;
     }
 
     private static List<Module> getAndClearModules(Context context) {
@@ -65,13 +93,14 @@ public class Timetable {
         for (String modCode : modSet) {
             mods.add(SampleModules.getModuleByCode(modCode));
         }
-        sharedPref.edit().clear().apply();
+        sharedPref.edit().remove("modules").apply();
         return mods;
     }
 
     public static void test(Context context) {
         List<Module> mods = getAndClearModules(context);
         Log.v("Timetable", mods.toString());
+        if (mods.size() == 0) return;
         Timetable t = new Timetable();
         boolean hasNext = t.next(mods);
         while (hasNext) {
@@ -80,6 +109,8 @@ public class Timetable {
         }
     }
 
+    @NonNull
+    @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
         for (Lesson l : table) {
