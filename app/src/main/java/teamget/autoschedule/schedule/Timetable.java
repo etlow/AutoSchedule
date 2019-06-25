ad56d2bd04d39dd6521cf0e7319279ee106b910a
etlow <42794246+etlow@users.noreply.github.com>
2019-06-25 12:31:58 +0800

    Change Timetable pos to array, rename fields

--- app/src/main/java/teamget/autoschedule/schedule/Timetable.java
+++ app/src/main/java/teamget/autoschedule/schedule/Timetable.java
@@ -16,26 +16,27 @@ import teamget.autoschedule.mods.Option;
 import teamget.autoschedule.mods.SampleModules;
 
 public class Timetable {
-    private int currList = -1;
-    private int numLessons = 0;
-    private List<Integer> pos = new ArrayList<>();
-    private Option[][] modules;
+    private int currPos = -1;
+    private int numOptions = 0;
+    private int[] pos;
+    private Option[][] optionsArr;
     private Lesson[] table;
 
     private void setModules(List<Module> modList) {
         for (int i = 0; i < modList.size(); i++) {
-            numLessons += modList.get(i).list.size();
+            numOptions += modList.get(i).list.size();
         }
-        table = new Lesson[7 * 24 * numLessons];
+        pos = new int[numOptions];
+        table = new Lesson[7 * 24 * numOptions];
 
-        modules = new Option[numLessons][];
+        optionsArr = new Option[numOptions][];
         int nextPos = 0;
         for (int i = 0; i < modList.size(); i++) {
             List<List<Option>> list = modList.get(i).list;
             for (int j = 0; j < list.size(); j++) {
                 List<Option> options = list.get(j);
                 Option[] optArr = new Option[options.size()];
-                modules[nextPos++] = optArr;
+                optionsArr[nextPos++] = optArr;
                 for (int k = 0; k < optArr.length; k++) {
                     optArr[k] = options.get(k);
                 }
@@ -46,28 +47,26 @@ public class Timetable {
     private boolean next() {
         boolean clash = false;
         do {
-            if (currList != -1) { // Not the first call
+            if (currPos != -1) { // Not the first call
                 // Keep removing elements from pos if it's already the last option
-                while (pos.get(currList) == modules[currList].length - 1) {
-                    removeAll(modules[currList][pos.get(currList)]);
-                    pos.remove(currList);
-                    if (currList == 0) return false; // No more possibilities
-                    currList--;
+                while (pos[currPos] == optionsArr[currPos].length - 1) {
+                    removeAll(optionsArr[currPos][pos[currPos]]);
+                    if (currPos == 0) return false; // No more possibilities
+                    currPos--;
                 }
                 // Update table to next option
-                int optNum = pos.get(currList);
-                removeAll(modules[currList][optNum]);
-                pos.set(currList, optNum + 1);
-                Option newOption = modules[currList][optNum + 1];
+                int optNum = pos[currPos]++;
+                removeAll(optionsArr[currPos][optNum]);
+                Option newOption = optionsArr[currPos][optNum + 1];
                 // Check for clashes if newOption is added
                 clash = clashes(newOption);
                 addAll(newOption);
             }
             // Keep adding 0 to pos until last list is reached
-            while (!clash && currList < numLessons - 1) {
-                pos.add(0);
-                currList++;
-                Option newOption = modules[currList][0];
+            while (!clash && currPos < numOptions - 1) {
+                currPos++;
+                pos[currPos] = 0;
+                Option newOption = optionsArr[currPos][0];
                 clash = clashes(newOption);
                 addAll(newOption);
             }
@@ -81,8 +80,8 @@ public class Timetable {
             int start = newLesson.day * 24 + newLesson.startHour;
             int end = newLesson.day * 24 + newLesson.endHour;
             for (int j = start; j < end; j++) {
-                for (int k = 0; k < numLessons; k++) {
-                    Lesson lesson = table[j * numLessons + k];
+                for (int k = 0; k < numOptions; k++) {
+                    Lesson lesson = table[j * numOptions + k];
                     if (lesson != null && newLesson.overlaps(lesson)) return true;
                 }
             }
@@ -96,7 +95,7 @@ public class Timetable {
             int start = newLesson.day * 24 + newLesson.startHour;
             int end = newLesson.day * 24 + newLesson.endHour;
             for (int j = start; j < end; j++) {
-                table[j * numLessons + currList] = newLesson;
+                table[j * numOptions + currPos] = newLesson;
             }
         }
     }
@@ -107,7 +106,7 @@ public class Timetable {
             int start = newLesson.day * 24 + newLesson.startHour;
             int end = newLesson.day * 24 + newLesson.endHour;
             for (int j = start; j < end; j++) {
-                table[j * numLessons + currList] = null;
+                table[j * numOptions + currPos] = null;
             }
         }
     }
@@ -135,7 +134,7 @@ public class Timetable {
         int i = 0;
         while (hasNext) {
             if (i % 1000000 == 0) {
-                Log.v("Timetable", t.pos.toString());
+                Log.v("Timetable", java.util.Arrays.toString(t.pos));
                 Log.v("Timetable", t.toString());
             }
             hasNext = t.next();
