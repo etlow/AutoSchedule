87f06f334142aabdef9093df4d4c46e0dd6b1471
Grant <grantlee97@gmail.com>
2019-07-01 01:07:15 +0800

    Shows list of 5 timetables

--- app/src/main/java/teamget/autoschedule/Top5Timetables.java
+++ app/src/main/java/teamget/autoschedule/Top5Timetables.java
@@ -73,57 +73,63 @@ public class Top5Timetables extends AppCompatActivity {
         new TimetableScoring(priorities).arrangeTimetablesByScore(timetables);
 
         // Present top 5 timetables
-        findViewById(R.id.buttonBasic).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Top5Timetables.this, TimetableDaily.class);
-                startActivity(intent);
-            }
-        });
 
-        GridLayout gridLayout = new GridLayout(getApplicationContext());
-        gridLayout.setColumnCount(12);
-        gridLayout.setRowCount(6);
-        gridLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
-        gridLayout.setAlignmentMode(GridLayout.ALIGN_BOUNDS);
-        for (int i = 0; i < 11; i++) {
-            TextView textView = new TextView(getApplicationContext());
-            textView.setText(Integer.toString(i + 8));
-            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-            params.rowSpec = GridLayout.spec(0, 1);
-            params.columnSpec = GridLayout.spec(i + 1, 1, 1);
-            params.setGravity(Gravity.FILL_HORIZONTAL);
-            textView.setLayoutParams(params);
-            gridLayout.addView(textView);
+        List<LinearLayout> layouts = new ArrayList<>();
+        LinearLayout linearLayout0 = findViewById(R.id.timetable0);
+        layouts.add(linearLayout0);
+        LinearLayout linearLayout1 = findViewById(R.id.timetable1);
+        layouts.add(linearLayout1);
+        LinearLayout linearLayout2 = findViewById(R.id.timetable2);
+        layouts.add(linearLayout2);
+        LinearLayout linearLayout3 = findViewById(R.id.timetable3);
+        layouts.add(linearLayout3);
+        LinearLayout linearLayout4 = findViewById(R.id.timetable4);
+        layouts.add(linearLayout4);
+
+        for (int grid = 0; grid < 5; grid++) {
+            GridLayout gridLayout = new GridLayout(getApplicationContext());
+            gridLayout.setColumnCount(12);
+            gridLayout.setRowCount(6);
+            gridLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+            gridLayout.setAlignmentMode(GridLayout.ALIGN_BOUNDS);
+            for (int i = 0; i < 11; i++) {
+                TextView textView = new TextView(getApplicationContext());
+                textView.setText(Integer.toString(i + 8));
+                GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+                params.rowSpec = GridLayout.spec(0, 1);
+                params.columnSpec = GridLayout.spec(i + 1, 1, 1);
+                params.setGravity(Gravity.FILL_HORIZONTAL);
+                textView.setLayoutParams(params);
+                gridLayout.addView(textView);
+            }
+            DayOfWeek[] days = DayOfWeek.values();
+            for (int i = 0; i < 5; i++) {
+                TextView textView = new TextView(getApplicationContext());
+                textView.setText(days[i].name().substring(0, 3));
+                textView.setGravity(Gravity.CENTER);
+                GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+                params.rowSpec = GridLayout.spec(i + 1, 1);
+                params.columnSpec = GridLayout.spec(0, 1);
+                params.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+                textView.setLayoutParams(params);
+                gridLayout.addView(textView);
+            }
+            for (Event event : timetables.get(grid).events) {
+                TextView textView = new TextView(getApplicationContext());
+                Lesson lesson = event.options.get(0).list.get(0);
+                textView.setText(lesson.moduleCode + "\n" + lesson.type.substring(0, 3) + "\n" + lesson.location.code);
+                textView.setTextSize(8);
+                textView.setGravity(Gravity.CENTER);
+                textView.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+                params.rowSpec = GridLayout.spec(event.day + 1, 1);
+                params.columnSpec = GridLayout.spec(event.startHour - 7, event.endHour - event.startHour);
+                params.setGravity(Gravity.FILL_HORIZONTAL);
+                textView.setLayoutParams(params);
+                gridLayout.addView(textView);
+            }
+            layouts.get(grid).addView(gridLayout);
         }
-        DayOfWeek[] days = DayOfWeek.values();
-        for (int i = 0; i < 5; i++) {
-            TextView textView = new TextView(getApplicationContext());
-            textView.setText(days[i].name().substring(0, 3));
-            textView.setGravity(Gravity.CENTER);
-            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-            params.rowSpec = GridLayout.spec(i + 1, 1);
-            params.columnSpec = GridLayout.spec(0, 1);
-            params.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-            textView.setLayoutParams(params);
-            gridLayout.addView(textView);
-        }
-        for (Event event : timetables.get(0).events) {
-            TextView textView = new TextView(getApplicationContext());
-            Lesson lesson = event.options.get(0).list.get(0);
-            textView.setText(lesson.moduleCode + "\n" + lesson.type.substring(0, 3) + "\n" + lesson.location.code);
-            textView.setTextSize(8);
-            textView.setGravity(Gravity.CENTER);
-            textView.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-            params.rowSpec = GridLayout.spec(event.day + 1, 1);
-            params.columnSpec = GridLayout.spec(event.startHour - 7, event.endHour - event.startHour);
-            params.setGravity(Gravity.FILL_HORIZONTAL);
-            textView.setLayoutParams(params);
-            gridLayout.addView(textView);
-        }
-        LinearLayout linearLayout = findViewById(R.id.timetableTest);
-        linearLayout.addView(gridLayout);
 
         // Include option to open another 5
     }
