a46743ee11856e105b4f07ad6fd2279b26e64874
etlow <42794246+etlow@users.noreply.github.com>
2019-07-27 01:21:29 +0800

    Change Top5Timetables to RecyclerView

---
+++ app/src/main/java/teamget/autoschedule/TimetableRecyclerAdapter.java
@@ -0,0 +1,119 @@
+package teamget.autoschedule;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.util.Consumer;
+import android.support.v7.widget.GridLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.time.DayOfWeek;
+import java.util.List;
+
+import teamget.autoschedule.mods.Lesson;
+import teamget.autoschedule.schedule.Event;
+import teamget.autoschedule.schedule.Timetable;
+
+class TimetableRecyclerAdapter extends RecyclerView.Adapter<TimetableRecyclerAdapter.MyViewHolder> {
+    private List<Timetable> timetables;
+    private Consumer<Integer> onButtonClick;
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+        private ConstraintLayout constraintLayout;
+        private MyViewHolder(ConstraintLayout l, Consumer<Integer> onButtonClick) {
+            super(l);
+            constraintLayout = l;
+            constraintLayout.getViewById(R.id.button)
+                    .setOnClickListener(view -> onButtonClick.accept(getAdapterPosition()));
+        }
+    }
+
+    TimetableRecyclerAdapter(List<Timetable> t, Consumer<Integer> c) {
+        timetables = t;
+        onButtonClick = c;
+    }
+
+    @Override
+    @NonNull
+    public TimetableRecyclerAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
+                                                                    int viewType) {
+        ConstraintLayout constraintLayout = (ConstraintLayout) LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.timetable_choice, parent, false);
+
+        return new MyViewHolder(constraintLayout, onButtonClick);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder viewHolder, int position) {
+
+        GridLayout gridLayout = viewHolder.constraintLayout.findViewById(R.id.gridLayout);
+        Context context = viewHolder.constraintLayout.getContext();
+        Timetable timetable = timetables.get(position);
+
+        int lastDay = 4;
+        int earliest = timetable.events.get(0).startHour;
+        int latest = timetable.events.get(0).endHour;
+        for (Event event : timetable.events) {
+            if (event.day > lastDay) lastDay = event.day;
+            if (event.startHour < earliest) earliest = event.startHour;
+            if (event.endHour > latest) latest = event.endHour;
+        }
+
+        gridLayout.removeAllViews();
+        gridLayout.setRowCount(lastDay + 2);
+        gridLayout.setColumnCount(latest - earliest + 1);
+        for (int i = 0; i < latest - earliest; i++) {
+            gridLayout.addView(makeTextView(context, Integer.toString(i + earliest),
+                    Gravity.NO_GRAVITY, GridLayout.spec(0, 1),
+                    GridLayout.spec(i + 1, 1, 1), Gravity.FILL_HORIZONTAL));
+        }
+        DayOfWeek[] days = DayOfWeek.values();
+        for (int i = 0; i <= lastDay; i++) {
+            gridLayout.addView(makeTextView(context, days[i].name().substring(0, 3), Gravity.CENTER,
+                    GridLayout.spec(i + 1, 1), GridLayout.spec(0, 1),
+                    Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL));
+        }
+        for (Event event : timetable.events) {
+            Lesson lesson = event.options.get(0).list.get(0);
+            TextView textView = makeTextView(context,
+                    lesson.moduleCode + "\n" + lesson.type.substring(0, 3)
+                            + "\n" + lesson.location.code,
+                    Gravity.CENTER, GridLayout.spec(event.day + 1, 1),
+                    GridLayout.spec(event.startHour - earliest + 1,
+                            event.endHour - event.startHour), Gravity.FILL_HORIZONTAL);
+            textView.setTextSize(8);
+            textView.setBackgroundColor(context.getResources().getColor(R.color.colorPrimary));
+            gridLayout.addView(textView);
+        }
+
+        String text = String
+                .format(context.getString(R.string.top5timetables_button_selecttimetable),
+                        position + 1);
+        ((Button) viewHolder.constraintLayout.getViewById(R.id.button)).setText(text);
+    }
+
+    @Override
+    public int getItemCount() {
+        return timetables.size();
+    }
+
+    private static TextView makeTextView(Context context, String text, int gravity,
+                                         GridLayout.Spec rowSpec, GridLayout.Spec columnSpec,
+                                         int paramGravity) {
+        TextView textView = new TextView(context);
+        textView.setText(text);
+        textView.setGravity(gravity);
+        GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+        params.rowSpec = rowSpec;
+        params.columnSpec = columnSpec;
+        params.setGravity(paramGravity);
+        textView.setLayoutParams(params);
+        return textView;
+    }
+}
--- app/src/main/java/teamget/autoschedule/Top5Timetables.java
+++ app/src/main/java/teamget/autoschedule/Top5Timetables.java
@@ -4,24 +4,18 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.time.DayOfWeek;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
-import teamget.autoschedule.mods.Lesson;
 import teamget.autoschedule.mods.Module;
 import teamget.autoschedule.mods.Option;
 import teamget.autoschedule.mods.SampleModules;
@@ -33,11 +27,12 @@ import teamget.autoschedule.schedule.TimetableScoring;
 import teamget.autoschedule.schedule.TypeAdapter;
 
 public class Top5Timetables extends AppCompatActivity {
-    Button select0, select1, select2, select3, select4;
-    int selectedTimetable;
-    List<Timetable> timetables;
-
     private static final int NUM_CONSIDER = 10;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    List<Timetable> timetables;
 
 //    SharedPreferences timetablePref;
 //    SharedPreferences.Editor spEditor;
@@ -125,96 +120,22 @@ public class Top5Timetables extends AppCompatActivity {
         // Calculate score, tag score, arrange in decreasing order
         new TimetableScoring(priorities).arrangeTimetablesByScore(timetables);
 
-        // Present top 5 timetables
+        // Present top timetables
+        recyclerView = findViewById(R.id.generatedTimetables);
+        recyclerView.setHasFixedSize(true);
 
-        List<LinearLayout> layouts = new ArrayList<>();
-        LinearLayout linearLayout0 = findViewById(R.id.timetable0);
-        layouts.add(linearLayout0);
-        LinearLayout linearLayout1 = findViewById(R.id.timetable1);
-        layouts.add(linearLayout1);
-        LinearLayout linearLayout2 = findViewById(R.id.timetable2);
-        layouts.add(linearLayout2);
-        LinearLayout linearLayout3 = findViewById(R.id.timetable3);
-        layouts.add(linearLayout3);
-        LinearLayout linearLayout4 = findViewById(R.id.timetable4);
-        layouts.add(linearLayout4);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
-        for (int grid = 0; grid < 5; grid++) {
-            GridLayout gridLayout = new GridLayout(getApplicationContext());
-            gridLayout.setColumnCount(12);
-            gridLayout.setRowCount(6);
-            gridLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
-            gridLayout.setAlignmentMode(GridLayout.ALIGN_BOUNDS);
-            for (int i = 0; i < 11; i++) {
-                TextView textView = new TextView(getApplicationContext());
-                textView.setText(Integer.toString(i + 8));
-                GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-                params.rowSpec = GridLayout.spec(0, 1);
-                params.columnSpec = GridLayout.spec(i + 1, 1, 1);
-                params.setGravity(Gravity.FILL_HORIZONTAL);
-                textView.setLayoutParams(params);
-                gridLayout.addView(textView);
-            }
-            DayOfWeek[] days = DayOfWeek.values();
-            for (int i = 0; i < 5; i++) {
-                TextView textView = new TextView(getApplicationContext());
-                textView.setText(days[i].name().substring(0, 3));
-                textView.setGravity(Gravity.CENTER);
-                GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-                params.rowSpec = GridLayout.spec(i + 1, 1);
-                params.columnSpec = GridLayout.spec(0, 1);
-                params.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-                textView.setLayoutParams(params);
-                gridLayout.addView(textView);
-            }
-            for (Event event : timetables.get(grid).events) {
-                TextView textView = new TextView(getApplicationContext());
-                Lesson lesson = event.options.get(0).list.get(0);
-                textView.setText(lesson.moduleCode + "\n" + lesson.type.substring(0, 3) + "\n" + lesson.location.code);
-                textView.setTextSize(8);
-                textView.setGravity(Gravity.CENTER);
-                textView.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-                GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-                params.rowSpec = GridLayout.spec(event.day + 1, 1);
-                params.columnSpec = GridLayout.spec(event.startHour - 7, event.endHour - event.startHour);
-                params.setGravity(Gravity.FILL_HORIZONTAL);
-                textView.setLayoutParams(params);
-                gridLayout.addView(textView);
-            }
-            layouts.get(grid).addView(gridLayout);
-        }
-
-        select0 = (Button) findViewById(R.id.button0);
-        select1 = (Button) findViewById(R.id.button1);
-        select2 = (Button) findViewById(R.id.button2);
-        select3 = (Button) findViewById(R.id.button3);
-        select4 = (Button) findViewById(R.id.button4);
-
-        // Include option to open another 5
+        adapter = new TimetableRecyclerAdapter(timetables, this::onButtonClick);
+        recyclerView.setAdapter(adapter);
     }
 
-    public void onButtonClick(View v) {
-        switch(v.getId()) {
-            case R.id.button0:
-                selectedTimetable = 0;
-                break;
-            case R.id.button1:
-                selectedTimetable = 1;
-                break;
-            case R.id.button2:
-                selectedTimetable = 2;
-                break;
-            case R.id.button3:
-                selectedTimetable = 3;
-                break;
-            case R.id.button4:
-                selectedTimetable = 4;
-        }
-
+    public void onButtonClick(int position) {
         final Gson gson = new Gson();
         getSharedPreferences("ChosenTimetable", MODE_PRIVATE)
                 .edit()
-                .putString("timetable", gson.toJson(timetables.get(selectedTimetable)))
+                .putString("timetable", gson.toJson(timetables.get(position)))
                 .apply();
         Intent intent = new Intent(this, ChosenTimetable.class);
         startActivity(intent);
