da0e10838d73afc6db21584532d76d9911aaf5ab
etlow <42794246+etlow@users.noreply.github.com>
2019-07-30 17:24:37 +0800

    Return a maximum distance for unknown locations

--- app/src/main/java/teamget/autoschedule/mods/Location.java
+++ app/src/main/java/teamget/autoschedule/mods/Location.java
@@ -3,22 +3,27 @@ package teamget.autoschedule.mods;
 import androidx.annotation.NonNull;
 
 public class Location {
-    private static double[][] distArr = {{0,457,520.9},{390.9,0,404.7},{503.2,471.5,0}};
+    private static final double[][] distArr = {{0,457,520.9},{390.9,0,404.7},{503.2,471.5,0}};
+    private static final double maxDist = 520.9;
+    private static final int UNKNOWN = -1;
     public String code;
     private int ref;
 
     public Location(String str) {
         code = str;
-        switch (str.substring(0, 3)) {
+        if (str.length() < 3) {
+            ref = UNKNOWN;
+        } else switch (str.substring(0, 3)) {
             case "UT-": ref = 0; break;
             case "ERC": ref = 0; break;
             case "TP-": ref = 0; break;
             case "COM": ref = 2; break;
-            default: ref = 0;
+            default: ref = UNKNOWN;
         }
     }
 
     public double distanceTo(Location other) {
+        if (ref == UNKNOWN || other.ref == UNKNOWN) return maxDist;
         return distArr[ref][other.ref];
     }
 
